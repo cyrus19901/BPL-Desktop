5dc2647c7cee42c427950a9e842bb7858e561cbc
Raj Singh <cyrus19901@macbook-pro.lan>
2018-07-05 22:26:33 -0700

    adding more fixes

--- client/app/src/services/time.service.js
+++ client/app/src/services/time.service.js
@@ -1,5 +1,6 @@
 ;(function () {
   'use strict'
+  const {app} = require('electron')
 
   angular.module('bplclient.services')
     .service('timeService', ['$q', '$http', '$interval', TimeService])
@@ -11,18 +12,16 @@
   function TimeService ($q, $http) {
     var timeServerUrl = 'http://tycho.usno.navy.mil/cgi-bin/time.pl'
     var serverLatency = 0
-    var args = {
-      headers: { "Content-Type": "application/json" },
-      data:{},
-      rejectUnauthorized: false
-    };
-    // var config = {
-    //   rejectUnauthorized: false,
-    //   requestCert: true,
-    //   agent: false,
-    //   insecure: true,
-    //   timeout: 2000
-    // }
+    // var args = {
+    //   headers: { "Content-Type": "application/json" },
+    //   data:{},
+    //   rejectUnauthorized: false
+    // };
+    var config = {
+      rejectUnauthorized: false,
+      insecure: true,
+      timeout: 2000
+    }
     var localToServerTimeDiff = 0
 
     /**
@@ -34,22 +33,22 @@
       var deferred = $q.defer()
 
       var startTime = new Date().getTime()
-
       $http.get(timeServerUrl, args).then(
         (success) => {
           console.log(success)
-          // var timestamp = success.headers().date
-          // var processedTimestamp = new Date(timestamp).getTime()
-          // var endTime = new Date().getTime()
+          var timestamp = success.headers().date
+          var processedTimestamp = new Date(timestamp).getTime()
+          var endTime = new Date().getTime()
 
-          // serverLatency = endTime - startTime
+          serverLatency = endTime - startTime
 
-          // var computedTimestamp = processedTimestamp + serverLatency
+          var computedTimestamp = processedTimestamp + serverLatency
 
-          // var currentLocalTime = new Date().getTime()
-          // localToServerTimeDiff = computedTimestamp - currentLocalTime
+          var currentLocalTime = new Date().getTime()
+          localToServerTimeDiff = computedTimestamp - currentLocalTime
 
-          // deferred.resolve(computedTimestamp)
+          deferred.resolve(computedTimestamp)
+
         },
         (_error) => {
           console.log("here")
@@ -61,7 +60,6 @@
           deferred.resolve(computedTimestamp)
         }
       )
-
       return deferred.promise
     }
 
