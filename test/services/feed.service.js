c7bdeb14d3fa87d75e826e4aa393ef127b6b1124
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-09-15 01:57:46 +0200

    fixes announcements feed

--- test/services/feed.service.js
+++ test/services/feed.service.js
@@ -15,7 +15,7 @@ describe('feedService', () => {
     it('fetches and parses the blockpool.io feed URL', () => {
       const stub = sinon.stub(feedService, 'fetchAndParse').resolves('OK')
       feedService.fetchBlogEntries()
-      expect(stub.firstCall.args[0]).to.eql('https://medium.com/blockpool')
+      expect(stub.firstCall.args[0]).to.eql('https://medium.com/feed/blockpool')
     })
   })
 })
