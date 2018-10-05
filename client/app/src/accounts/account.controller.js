e7f95e6cd15612c2af0433716bc32fab0c79755c
Raj Singh <cyrus19901@macbook-pro.lan>
2018-10-04 22:20:17 -0700

    making the symbol of BTC more clear and other fixes

--- client/app/src/accounts/account.controller.js
+++ client/app/src/accounts/account.controller.js
@@ -101,7 +101,7 @@
     pluginLoader.triggerEvent('onStart')
 
     self.currencies = [
-      { name: 'btc', symbol: 'Ƀ' },
+      { name: 'btc', symbol: '₿'  },
       { name: 'usd', symbol: '$' },
       { name: 'aud', symbol: 'A$' },
       { name: 'brl', symbol: 'R$' },
